c3b7919c3d9f90f4231485b629a13e9743862535
hojjatre <hojjat7878.h@gmail.com>
2023-09-14 17:45:37 +0330

    user + role model and connection to database

---
+++ src/main/java/com/example/map/model/Role.java
@@ -0,0 +1,25 @@
+package com.example.map.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name")
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}

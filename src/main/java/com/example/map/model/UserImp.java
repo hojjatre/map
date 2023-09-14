151f158c6c27b9ce7251a03912b9cdcc4a3f029d
hojjatre <hojjat7878.h@gmail.com>
2023-09-14 17:45:37 +0330

    user + role model and connection to database

---
+++ src/main/java/com/example/map/model/UserImp.java
@@ -0,0 +1,63 @@
+package com.example.map.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        }
+)
+@NoArgsConstructor
+@NamedEntityGraph(
+        name = "graph.user",
+        attributeNodes = {
+                @NamedAttributeNode("username"),
+                @NamedAttributeNode("email"),
+                @NamedAttributeNode(value = "roles", subgraph = "graph.role")
+        },
+        subgraphs = {
+                @NamedSubgraph(
+                        name = "graph.role",
+                        attributeNodes = {
+                                @NamedAttributeNode("name")
+                        }
+                )
+        }
+)
+public class UserImp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "username")
+    private String username;
+    @Email
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {
+                    @JoinColumn(name = "user_id")
+            },
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public UserImp(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}

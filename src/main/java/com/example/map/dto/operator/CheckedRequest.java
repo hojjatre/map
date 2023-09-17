c76867cf523301c371367bc1f6b061b8c52cc3d0
hojjatre <hojjat7878.h@gmail.com>
2023-09-17 11:25:57 +0330

    add limitation for user + operator check the reports

---
+++ src/main/java/com/example/map/dto/operator/CheckedRequest.java
@@ -0,0 +1,18 @@
+package com.example.map.dto.operator;
+
+import com.example.map.model.EReport;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckedRequest {
+    private String username;
+    private String coordinate;
+    private String reportType;
+    private String reportData;
+}
